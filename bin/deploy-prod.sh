f4a8d9279b251f55a46c56f3873da8e277b988ac
wit <witwitchayakarn@gmail.com>
2025-06-05 13:26:39 +0700

    Update the deploy script

--- bin/deploy-prod.sh
+++ bin/deploy-prod.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-aws s3 sync out/_next s3://justnote-docs/_next --size-only --delete --cache-control max-age=31536000 --profile justnote
+aws s3 sync out/_next s3://justnote-docs/_next --size-only --cache-control max-age=31536000 --profile justnote
 
-aws s3 sync out/nextImageExportOptimizer s3://justnote-docs/nextImageExportOptimizer --size-only --delete --cache-control max-age=31536000 --profile justnote
+aws s3 sync out/nextImageExportOptimizer s3://justnote-docs/nextImageExportOptimizer --size-only --cache-control max-age=31536000 --profile justnote
 
-aws s3 sync out s3://justnote-docs --exclude "_next/*" --exclude "nextImageExportOptimizer/*" --size-only --delete --cache-control max-age=86400 --profile justnote
+aws s3 sync out s3://justnote-docs --exclude "_next/*" --exclude "nextImageExportOptimizer/*" --delete --cache-control max-age=86400 --profile justnote
 
 aws cloudfront create-invalidation --distribution-id E3HANMO243DZ3Y --paths "/*" --profile justnote
